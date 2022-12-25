944d0b7fcb689b5362f15cfc7399778f6472bf67
Gelfeto Gebre <gelf.learing@gmail.com>
2022-12-25 19:22:03 +0000

    printf

---
+++ pnums.c
@@ -0,0 +1,78 @@
+#include "main.h"
+/**
+ * print_binary - Print a number in base 2
+ * @list: Number to print in base 2
+ *
+ * Return: Length of the numbers in binary
+ **/
+int print_binary(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, unsigned int), 2);
+
+	size = print(p_buff);
+
+	return (size);
+}
+
+
+/**
+ * print_hexadecimal_low - Print a number in hexadecimal format
+ * @list: Number to print
+ *
+ * Return: Length of the number
+ **/
+int print_hexadecimal_low(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, unsigned int), 16);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
+}
+
+/**
+ * print_integer - Print a number in base 10
+ * @list: Number to print in base 10
+ *
+ * Return: Length of th numbers in decimal
+ **/
+int print_integer(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, int), 10);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
+}
+
+
+
+/**
+ * print_octal - Print a unsigned octal
+ * @list: Number to print
+ *
+ * Return: Length of the number
+ **/
+int print_octal(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, unsigned int), 8);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
+}
+
+
+
---
+++ string_unsigned.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_unsigned - Print a unsigned int
+ * @list: Number to print
+ *
+ * Return: Length of the number
+ **/
+int print_unsigned(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, unsigned int), 10);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
+}
+
+
+/**
+ * print_string - Print string
+ * @list: list.
+ *
+ * Return: String length.
+ */
+
+int print_string(va_list list)
+{
+	char *p;
+	int p_len;
+
+	p = va_arg(list, char*);
+	p_len = print((p != NULL) ? p : "(null)");
+
+	return (p_len);
+}
+
+
